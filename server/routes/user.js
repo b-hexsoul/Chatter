208e80dfef570baa27cc6b46cce4952b2ca45918
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-04-10 00:02:56 -0700

    'User router'

---
+++ server/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { getUserSearch } = require("../controllers/user");
+
+// Route of /api/user
+router.route("/").get(getUserSearch);
+
+module.exports = router;
