ad4a389ae1635be90db9f1d8c9a6fdd577d00906
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-04-15 00:01:48 -0700

    'Add BadgeAvatar component and user pictures'

---
+++ client/src/components/Messenger/BadgeAvatar.component.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Badge from "@material-ui/core/Badge";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+}))(Badge);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+  },
+}));
+
+export default function BadgeAvatars({ img, name }) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <StyledBadge
+          overlap="circle"
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          variant="dot"
+        >
+          <Avatar alt={name} src={img} className={classes.avatar} />
+        </StyledBadge>
+      </div>
+    </div>
+  );
+}
