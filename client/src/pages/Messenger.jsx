c3e6e4ebd7442254b4158b3be39a91e08a0bf1cc
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-04-30 21:03:07 -0700

    'Update Messenger page'

--- client/src/pages/Messenger.jsx
+++ client/src/pages/Messenger.jsx
@@ -1,4 +1,11 @@
-import { AppBar, Container, CssBaseline, Grid, Menu } from "@material-ui/core";
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  ListItem,
+  ListItemAvatar,
+  Typography,
+} from "@material-ui/core";
 import { useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,18 +14,17 @@ import UserList from "../components/Messenger/UserList.component";
 import ChatBox from "../components/Messenger/ChatBox.component";
 import SearchBar from "../components/Messenger/SearchBar.component";
 import BadgeAvatars from "../components/Messenger/BadgeAvatar.component";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import MessengerMenu from "../components/Messenger/MessengerMenu.component";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "100vh",
-    "& .MuiInput-underline:before": {
-      borderBottom: "1.2px solid rgba(0, 0, 0, 0.2)",
-    },
+    backgroundColor: "#fff",
   },
   currentUserCard: {
     border: "none",
     boxShadow: "none",
+    display: "flex",
   },
 }));
 
@@ -32,24 +38,31 @@ const Messenger = () => {
   }, [user, history]);
 
   return (
-    <Grid container component="main" spacing={2} className={classes.root}>
+    <Grid container component="main" spacing={1} className={classes.root}>
       <CssBaseline />
 
       <Grid item md={4}>
         <Container>
-          <AppBar position="relative" className={classes.currentUserCard}>
-            <BadgeAvatars name={"thomas"} img={"/images/thomas.png"} />
-            {"thomas"} {/* username */}
-          </AppBar>
+          <ListItem className={classes.currentUserCard}>
+            <ListItemAvatar>
+              <BadgeAvatars name={"thomas"} img={"/images/thomas.png"} />
+            </ListItemAvatar>
+            <Typography>thomas {/* current user username */}</Typography>
+            <MessengerMenu />
+          </ListItem>
 
-          <Menu>{/* put logout here */}</Menu>
+          <div style={{ "margin-top": 12, "margin-bottom": 12 }}>
+            <Typography variant="h5">Chats</Typography>
+          </div>
           <SearchBar />
           <UserList />
         </Container>
       </Grid>
 
       <Grid item md={8}>
-        <ChatBox />
+        <Container>
+          <ChatBox />
+        </Container>
       </Grid>
     </Grid>
   );
